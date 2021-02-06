edfcaa8a0de898d0c288ea5480ab30c584dacdd8
ultimateshark <manishpandey8858@gmail.com>
2021-02-06 15:55:51 +0530

    Change

--- app/__init__.py
+++ app/__init__.py
@@ -6,9 +6,9 @@ from .main.controller.user_controller import api as user_ns
 blueprint = Blueprint('api', __name__)
 
 api = Api(blueprint,
-          title='FLASK RESTPLUS API BOILER-PLATE WITH JWT',
+          title='Project Simplex',
           version='1.0',
-          description='a boilerplate for flask restplus web service'
+          description='a project management tool for milenials'
           )
 
 api.add_namespace(user_ns, path='/user')
